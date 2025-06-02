0e502fa182dd1b9db231401447fdc6446c913cdc
Jakub Mikłasz <jmiklasz21@gmail.com>
2025-06-02 17:11:44 +0200

    Integrate endpoint path

--- src/main/kotlin/com/taxiapp/driverpaycheck/controllers/PaycheckController.kt
+++ src/main/kotlin/com/taxiapp/driverpaycheck/controllers/PaycheckController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.security.Principal
 
-@Controller("/paycheck")
+@Controller("/api/paycheck")
 class PaycheckController(
     private val paycheckService: PaycheckService
 ) {
